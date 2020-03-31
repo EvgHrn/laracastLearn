6c91764f066ab918ba387eab71d2b2240ba58a89
EvgHrn <evg.hrn@gmail.com>
2020-03-31 20:25:11 +0400

    Ep 22 First DI

--- controllers/index.php
+++ controllers/index.php
@@ -1,6 +1,5 @@
 <?php
 
-// $tasks = $app['database']->selectAll('todos');
-$users = $app['database']->selectAll('users');
+$users = App::get('database')->selectAll('users');
 
 require 'views/index.view.php';
